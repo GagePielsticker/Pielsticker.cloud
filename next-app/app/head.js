31003365a646a04a43970beb495a96968e99a05e
James pielsticker <gagepays@gmail.com>
2023-08-24 17:43:50 -0500

    Update head.js

--- next-app/app/head.js
+++ next-app/app/head.js
@@ -3,9 +3,11 @@ export default function Head() {
         <>
         <title>JGP Cloud Portfolio</title>
 
+        <meta name="og:site_name" content="JGP Cloud Portfolio"></meta>
         <meta property="og:title" content="Pielsticker.cloud | Projects, Solutions & Consultation." />
         <meta property="og:type" content="website" />
         <meta property="og:url" content="https://pielsticker.cloud" />
+        <meta property="og:image" content="/icon.png" ></meta>
         <meta name='url' content='https://pielsticker.cloud'></meta>
         
         <meta property="og:description" content="A portfolio website for James Pielsticker's various programming projects. Mostly based in AWS cloud first approaches." />
